59d4802946c180218d6488b2064ef4601ca4bbd8
saurav82923 <saurav14032000@gmail.com>
2023-08-24 01:37:23 +0530

    level order traverssal
    gf

---
+++ LeetcodeArray/example.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int ans=0;
+    for(int i=0;i<3;i++){
+        int a;
+        cin>>a;
+        ans=a*
+        +-
+        pow(10,i)+ ans;
+    }
+    cout<<ans;
+ return 0;
+}
